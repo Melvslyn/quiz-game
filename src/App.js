2ce836fd56547f36a45d52f64c9273d227d3317a
Melvin Gamana Jr <melvingamanajr@gmail.com>
2023-05-02 21:30:10 +0800

    adding more questions each categories

--- src/App.js
+++ src/App.js
@@ -6,16 +6,148 @@ export default function App() {
 
     const categories = ["Python", "Java", "C#"];
 
-    const pythonQuestions = [        {            questionText: 'Which of the following is not a data type in Python?',            answerOptions: [                { answerText: 'integer', isCorrect: false },                { answerText: 'float', isCorrect: false },                { answerText: 'string', isCorrect: false },                { answerText: 'character', isCorrect: true },            ],
-        }
+    const pythonQuestions = [        
+      {            
+        questionText: 'Which of the following is not a data type in Python?',
+        answerOptions: [                
+          { answerText: 'integer', isCorrect: false }, 
+          { answerText: 'float', isCorrect: false },                
+          { answerText: 'string', isCorrect: false },                
+          { answerText: 'character', isCorrect: true },            
+        ],
+        },
+        {            
+          questionText: 'Which keyword is used to create a function in Python?',
+          answerOptions: [                
+            { answerText: 'func', isCorrect: false }, 
+            { answerText: 'function', isCorrect: false },                
+            { answerText: 'def', isCorrect: true },                
+            { answerText: 'define', isCorrect: false },            
+          ],
+          },
+          {            
+            questionText: 'What is the correct way to import the "math" module in Python?',
+            answerOptions: [                
+              { answerText: 'import Math', isCorrect: true }, 
+              { answerText: 'from math import *', isCorrect: false },                
+              { answerText: 'include math', isCorrect: false },                
+              { answerText: 'from math import math', isCorrect: false },            
+            ],
+            },
+            {            
+              questionText: 'Which of the following is not a loop statement in Python?',
+              answerOptions: [                
+                { answerText: 'for loop', isCorrect: false }, 
+                { answerText: 'while loop', isCorrect: false },                
+                { answerText: 'do-while loop', isCorrect: true },                
+                { answerText: 'range loop', isCorrect: false },            
+              ],
+              },
+              {            
+                questionText: 'Which of the following is not a data type in Python?',
+                answerOptions: [                
+                  { answerText: 'integer', isCorrect: false }, 
+                  { answerText: 'float', isCorrect: false },                
+                  { answerText: 'string', isCorrect: false },                
+                  { answerText: 'character', isCorrect: true },            
+                ],
+                }
     ];
 
-    const javaQuestions = [        {            questionText: 'What is the output of the following code in Java? \n System.out.println(5/2);',            answerOptions: [                { answerText: '2', isCorrect: false },                { answerText: '2.5', isCorrect: false },                { answerText: '2.0', isCorrect: true },                { answerText: '5/2', isCorrect: false },            ],
-        }
+    const javaQuestions = [
+      {            
+        questionText: 'Which of the following is not a valid data type in Java?',
+        answerOptions: [                
+          { answerText: 'int', isCorrect: false },                
+          { answerText: 'float', isCorrect: false },                
+          { answerText: 'double', isCorrect: false },                
+          { answerText: 'decimal', isCorrect: true },            
+        ],
+        },
+        {            
+          questionText: 'What is the output of the following code in Java? \n System.out.println(5/2);',
+          answerOptions: [                
+            { answerText: '2', isCorrect: false },                
+            { answerText: '2.5', isCorrect: false },                
+            { answerText: '2.0', isCorrect: true },                
+            { answerText: '5/2', isCorrect: false },            
+          ],
+          },
+          {            
+            questionText: 'What is the output of the following code? \n String name = "John"; \n System.out.println("Hello " + name);',
+            answerOptions: [                
+              { answerText: 'Hello John', isCorrect: true },                
+              { answerText: 'John', isCorrect: false },                
+              { answerText: 'Hello', isCorrect: false },                
+              { answerText: 'None of the above', isCorrect: false },            
+            ],
+            },
+            {            
+              questionText: 'What is the output of the following code? int i = 0; \n while (i < 5) { \n System.out.println(i); \n i++; \n }',
+              answerOptions: [                
+                { answerText: '0 1 2 3 4', isCorrect: true },                
+                { answerText: '1 2 3 4 5', isCorrect: false },                
+                { answerText: '0 1 2 3', isCorrect: false },                
+                { answerText: 'None of the above', isCorrect: false },            
+              ],
+              },
+              {            
+                questionText: 'What is the output of the following code in Java? \n System.out.println(5/2);',
+                answerOptions: [                
+                  { answerText: '2', isCorrect: false },                
+                  { answerText: '2.5', isCorrect: false },                
+                  { answerText: '2.0', isCorrect: true },                
+                  { answerText: '5/2', isCorrect: false },            
+                ],
+                }
     ];
 
-    const cSharpQuestions = [        {            questionText: 'Which of the following is not a valid type in C#?',            answerOptions: [                { answerText: 'int', isCorrect: false },                { answerText: 'bool', isCorrect: false },                { answerText: 'string', isCorrect: false },                { answerText: 'float', isCorrect: true },            ],
-        }
+    const cSharpQuestions = [        
+      {            
+        questionText: 'Which of the following is not a valid type in C#?',
+        answerOptions: [                
+          { answerText: 'int', isCorrect: false },                
+          { answerText: 'bool', isCorrect: false },                
+          { answerText: 'string', isCorrect: false },                
+          { answerText: 'float', isCorrect: true },            
+        ],
+        },
+        {            
+          questionText: 'Which of the following is not a valid type in C#?',
+          answerOptions: [                
+            { answerText: 'int', isCorrect: false },                
+            { answerText: 'bool', isCorrect: false },                
+            { answerText: 'string', isCorrect: false },                
+            { answerText: 'float', isCorrect: true },            
+          ],
+          },
+          {            
+            questionText: 'Which of the following is not a valid type in C#?',
+            answerOptions: [                
+              { answerText: 'int', isCorrect: false },                
+              { answerText: 'bool', isCorrect: false },                
+              { answerText: 'string', isCorrect: false },                
+              { answerText: 'float', isCorrect: true },            
+            ],
+            },
+            {            
+              questionText: 'Which of the following is not a valid type in C#?',
+              answerOptions: [                
+                { answerText: 'int', isCorrect: false },                
+                { answerText: 'bool', isCorrect: false },                
+                { answerText: 'string', isCorrect: false },                
+                { answerText: 'float', isCorrect: true },            
+              ],
+              },
+              {            
+                questionText: 'Which of the following is not a valid type in C#?',
+                answerOptions: [                
+                  { answerText: 'int', isCorrect: false },                
+                  { answerText: 'bool', isCorrect: false },                
+                  { answerText: 'string', isCorrect: false },                
+                  { answerText: 'float', isCorrect: true },            
+                ],
+                }
     ];
 
     const handleCategorySelection = (category) => {
